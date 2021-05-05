53108708665c9e136e50b58155471aa7a23d589c
jotapecds <jpcoelhodesouza@gmail.com>
2021-05-05 17:30:12 -0300

    exercicio 2 incompleto

--- Exercicio2/teamQueue.c
+++ Exercicio2/teamQueue.c
@@ -14,11 +14,11 @@ struct Queue {
 
 struct Team {
     int team_index;
-    int num_elements;
+    int num_of_elements;
     int* elements;
 } typedef team_t;
 
-node_t* create_new_node(int value) {
+node_t* create_new_node(int value){
   node_t *new_node = (node_t*) malloc(sizeof(node_t));
   new_node->value = value;
   new_node->next = NULL;
@@ -27,29 +27,34 @@ node_t* create_new_node(int value) {
 
 queue_t* create_new_queue(){
     queue_t *new_queue = (queue_t*) malloc(sizeof(queue_t));
-    new_queue->head = new_queue->tail = NULL;
+    new_queue->head = NULL;
+    new_queue->tail = NULL;
     return new_queue;
 }
 
 void enqueue(queue_t* queue, int value){
-    node_t *temp = create_new_node(value);
-  
-    // Se a fila estiver vazia, o elemento inserido será tanto o primeiro quanto o último da fila
-    if (queue->tail == NULL) {
-        queue->head = queue->tail = temp;
-        return;
-    }
-  
-    // Caso contrário, a cauda atual passa a apontar para a nova cauda
-    queue->tail->next = temp;
-    queue->tail = temp;
+    node_t *new_node = create_new_node(value);
+
+    // Se a cabeça for nula insere o novo elemento no começo da fila
+    if(queue->head == NULL)
+        queue->head = new_node;
+
+    // Se a cauda não for nula significa que que já existe ao menos 1 elemento na fila, dessa forma podendo linka-lo com a nova cauda inserida
+    if (queue->tail != NULL){
+        queue->tail->next = new_node;
+    } 
+    
+    printf("\nEnfileirando %d", new_node->value);
+    queue->tail = new_node;
 }
 
 void dequeue(queue_t* queue){
-    if (queue->head == NULL) return;
+    if (queue->head == NULL) return; 
   
-    // Armazena a cabeça atual e move a fila um nó para frente
+    // Salva a cabeça atual para liberar a memória e move a fila um nó para frente
     node_t* temp = queue->head;
+
+    printf("\nRemovendo: %d", queue->head->value);
     queue->head = queue->head->next;
   
     // Caso a nova cabeça seja nula, significa que a fila está vazia
@@ -59,49 +64,66 @@ void dequeue(queue_t* queue){
     free(temp);
 }
 
-void printList(element_t *head){
-    element_t* temp = head;
+void fill_teams(team_t *teams[], int num_teams){
+    int num_of_elements;
 
+    for (int i = 0; i < num_teams; i++){
+
+        scanf("%d", &num_of_elements);
+        team_t *new_team = (team_t*) malloc(sizeof(team_t));
+
+        new_team->team_index = i;
+        new_team->num_of_elements = num_of_elements;
+        new_team->elements = (int*) malloc(sizeof(int) * num_of_elements);
+
+        for (int j = 0, element; j < num_of_elements; j++){
+            scanf("%d", &element);
+            new_team->elements[j] = element;
+        }
+
+        teams[i] = new_team;
+    }
+}
+
+void print_queue(queue_t *queue){
+    node_t* temp = queue->head;
+
+    printf("\nMinha fila:\n");
     while(temp != NULL){
         printf(" -> %d", temp->value);
-        temp = temp->next;    
+        temp = temp->next;
     }
 }
 
 int main(void){
+    int scenario = 1, num_teams;
 
-    element_t *head, e1, e2, e3;
+    do {
+        printf("Scenario #%d\n", scenario);
 
-    e1.value = 24;
-    e2.value = 10;
-    e3.value = 54;
+        // Lendo a quantidade de times e estabelecendo o critério de parada do programa
+        scanf("%d", &num_teams);
+        printf("%d", num_teams);
 
-    head = &e3;
-    e3.next = &e2;
-    e2.next = &e1;
-    e1.next = NULL;
+        if(num_teams == 0) exit(0);
 
-    printList(head);
+        // Criando a estrutura de times
+        team_t *teams[num_teams];
+        teams[num_teams] = (team_t*) malloc(sizeof(team_t) * num_teams);
 
+        queue_t *queue = create_new_queue();
 
-    // int tam_total_vet = 10000,
-    //     tam_atual_vet = 0,
-    //     linha,
-    //     *vet = (int *) malloc(tam_total_vet * sizeof(int));
+        fill_teams(teams, num_teams);
+        //commandsLoop(queue, teams);
 
-    // if(vet == NULL){   
-    //     printf("Erro! Memoria insuficiente");
-    //     return 0;
-    // }
+        // Libera o espaco dos ponteiros usados
+        free(queue);
+        free(teams[num_teams]);
 
-    // while (scanf("%d", &linha) == 1){
-    //     vet[tam_atual_vet] = linha;
-    //     tam_atual_vet++;
-    // }
-
-    // for(int i=0; i<tam_atual_vet; i++){
-    //     printf("Linha %d -> %d \n", i, vet[i]);
-    // }
+        // Incrementa o cenario
+        scenario++;
+        printf("\n");
+    } while (num_teams != 0);
 
     return 0;
 }
