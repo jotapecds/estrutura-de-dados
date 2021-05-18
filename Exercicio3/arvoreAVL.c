80119d2eb6cd4814248dfe95d53bcd66712529bb
jotapecds <jpcoelhodesouza@gmail.com>
2021-05-18 15:18:42 -0300

    Exercicio 3 completo

--- Exercicio3/arvoreAVL.c
+++ Exercicio3/arvoreAVL.c
@@ -12,9 +12,9 @@ int maior(int x, int y);
 int encontrar_altura(struct NO *no);
 int encontrar_fator_de_balanceamento(struct NO *no);
 struct NO* criar_novo_no(int info);
-struct NO* rodar_a_direita(struct NO *y);
-struct NO *rodar_a_esquerda(struct NO *x);
+struct NO* rodar_no(struct NO *raiz, char direcao);
 struct NO* inserir(struct NO* no, int info);
+void atualizar_altura(struct NO **no);
 void imprimeNo(struct NO *raiz);
 
 int main(void){
@@ -30,7 +30,8 @@ int main(void){
 }
 
 int maior(int x, int y){
-	return (x > y)? x : y;
+	if(x > y) return x;
+	else return y;
 }
 
 int encontrar_altura(struct NO *no){
@@ -45,75 +46,82 @@ int encontrar_fator_de_balanceamento(struct NO *no){
 	return encontrar_altura(no->esq) - encontrar_altura(no->dir);
 }
 
+void atualizar_altura(struct NO **no){
+	(*no)->alt = maior(encontrar_altura((*no)->esq), encontrar_altura((*no)->dir)) + 1;
+	return;
+}
+
 struct NO* criar_novo_no(int info){
 	struct NO *novo_no = (struct NO*) malloc(sizeof(struct NO));
 	novo_no->info = info;
 	novo_no->esq = NULL;
 	novo_no->dir = NULL;
-	novo_no->alt = 1; // o novo nó é inicialmente alocado como uma folha
+	novo_no->alt = 1;
 	return(novo_no);
 }
 
-struct NO* rodar_a_direita(struct NO *y){
-	struct NO *x = y->esq;
-	struct NO *T2 = x->dir;
+struct NO* rodar_no(struct NO *raiz, char direcao){
+	struct NO *nova_raiz;
+	struct NO *aux;
 
-	x->dir = y;
-	y->esq = T2;
+	if(direcao == 'd'){
+		// Rotação à direita
+		nova_raiz = raiz->esq;
+		aux = nova_raiz->dir;
 
-	y->alt = maior(encontrar_altura(y->esq), encontrar_altura(y->dir))+1;
-	x->alt = maior(encontrar_altura(x->esq), encontrar_altura(x->dir))+1;
+		nova_raiz->dir = raiz;
+		raiz->esq = aux;
+	}
+	else{
+		// Rotação à esquerda
+		nova_raiz = raiz->dir;
+		aux = nova_raiz->esq;
 
-	return x;
-}
+		nova_raiz->esq = raiz;
+		raiz->dir = aux;
+	}
 
-struct NO *rodar_a_esquerda(struct NO *x){
-	struct NO *y = x->dir;
-	struct NO *T2 = y->esq;
+	atualizar_altura(&raiz);
+	atualizar_altura(&nova_raiz);
 
-	y->esq = x;
-	x->dir = T2;
-
-	x->alt = maior(encontrar_altura(x->esq), encontrar_altura(x->dir))+1;
-	y->alt = maior(encontrar_altura(y->esq), encontrar_altura(y->dir))+1;
-
-	return y;
+	return nova_raiz;
 }
 
 struct NO* inserir(struct NO* no, int info){
-	if (no == NULL)
-		return(criar_novo_no(info));
+	if(no == NULL)
+		return criar_novo_no(info);
 
-	if (info < no->info)
+	if(info < no->info)
 		no->esq = inserir(no->esq, info);
-	else if (info > no->info)
+
+	else if(info > no->info)
 		no->dir = inserir(no->dir, info);
-	else // Equal keys are not allowed in BST
+
+	else
 		return no;
 
-	// Atualiza a altura do nó ancestral
-	no->alt = 1 + maior(encontrar_altura(no->esq), encontrar_altura(no->dir));
+	atualizar_altura(&no);
 
 	int fator_de_balanceamento = encontrar_fator_de_balanceamento(no);
 
 	// Balanceamento LL
-	if (fator_de_balanceamento > 1 && info < no->esq->info)
-		return rodar_a_direita(no);
+	if(fator_de_balanceamento > 1 && info < no->esq->info)
+		return rodar_no(no, 'd');
 
 	// Balanceamento RR
-	if (fator_de_balanceamento < -1 && info > no->dir->info)
-		return rodar_a_esquerda(no);
+	if(fator_de_balanceamento < -1 && info > no->dir->info)
+		return rodar_no(no, 'e');
 
 	// Balanceamento LR
-	if (fator_de_balanceamento > 1 && info > no->esq->info){
-		no->esq = rodar_a_esquerda(no->esq);
-		return rodar_a_direita(no);
+	if(fator_de_balanceamento > 1 && info > no->esq->info){
+		no->esq = rodar_no(no->esq, 'e');
+		return rodar_no(no, 'd');
 	}
 
 	// Balanceamento RL
-	if (fator_de_balanceamento < -1 && info < no->dir->info){
-		no->dir = rodar_a_direita(no->dir);
-		return rodar_a_esquerda(no);
+	if(fator_de_balanceamento < -1 && info < no->dir->info){
+		no->dir = rodar_no(no->dir, 'd');
+		return rodar_no(no, 'e');
 	}
 
 	return no;
